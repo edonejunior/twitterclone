f6c91eca25b6d06284eb303cc1a4c440c8c1d9e8
Edvaldo Domingues de Faria Junior <edonejunior@hotmail.com>
2020-07-16 10:35:00 -0300

    ProfilePage

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## Projeto
 
 Iniciado com TypeScript.
-Utilizado os components:
+Aprendi bastante a respeito de StyledComponents e sem dúvida aprendi a respeito de personalização de CSS.
+Também separei o projeto em componentes e foi criado dois arquivos .css para estilizar globalmente, o GlobalStyles e o Icons.ts.
+Utilizado também o Styled-Icons.
+
--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -14,9 +14,70 @@ export const Container = styled.div`
       display: none;
   }
 `;
-export const Banner = styled.div``;
-export const Avatar = styled.div``;
-export const ProfileData = styled.div``;
+export const Banner = styled.div`
+  flex-shrink:0;
+
+  width: 100%;
+  height: min(33vw, 199px);
+
+  background: var(--twitter);
+
+  position:relative;
+`;
+export const Avatar = styled.div`
+/* Deixa a bola responsiva */
+  width: max(45px, min(135px, 22vw));
+  height: max(45px, min(135px, 22vw));
+
+  border: 3.75px solid var(--primary);
+  background: var(--gray);
+  border-radius:50%;
+
+  position: absolute;
+  bottom: max(-50px, -10vw);
+  left: 15px;
+
+`;
+export const ProfileData = styled.div`
+
+  padding: min(calc(10vw + 7px), 67px) 16px 0;
+
+  display: flex;
+  flex-direction: column;
+
+  position:relative;
+
+  >h1 {
+    font-weight: bold;
+    font-size: 19px;
+  }
+  >h2 {
+    font-weight: normal;
+    font-size: 15px;
+
+    color: var(--gray);
+  }
+  >p {
+    font-size:15px;
+    margin-top: 11px;
+  }
+
+  >ul {
+    list-style:none;
+    margin-top: 10px;
+    margin-bottom: 10px;
+
+    >li {
+     
+      font-size: 15px;
+      color: var(--gray);
+      > svg {
+        fill: var(--gray);
+        margin-right: 5px;
+      }
+    }
+  }
+`;
 
 const iconCss = css`
 width: 20px;
@@ -28,4 +89,16 @@ color: var(--gray);
 export const LocationIcon = styled(LocationOn)`${iconCss}`;
 export const CakeIcon = styled(Cake)`${iconCss}`;
 
-export const Followage = styled.div``;
+export const Followage = styled.div`
+  display:flex;
+
+  >span {
+    font-size:15px;
+    color: var(--gray);
+
+    & + span {
+      margin-left: 20px;
+    }
+  }
+
+`;
